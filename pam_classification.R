dc7ef7490be81ce71df84d1694ecbe3a9f331a63
shermanip <sherman.ip@spc.ox.ac.uk>
2016-02-24 11:12:32 +0000

    Added justification for using 2 significant figures.

--- pam_classification.R
+++ pam_classification.R
@@ -1,5 +1,7 @@
 #load library for PAM
 library(pamr);
+#load library for cum. sd.
+library(TTR);
 
 #NOTE FOR pamr: need to convert to data.frame then to matrix
 
@@ -25,7 +27,7 @@ X_bootstrap = X;
 Y_bootstrap = Y;
 
 #for n_repeat times
-n_repeat = 100;
+n_repeat = 500;
 test_error_array = rep(0,n_repeat); #array of test error for every repeat of the experiment
 for (i in 1:n_repeat){
 
@@ -42,9 +44,24 @@ for (i in 1:n_repeat){
   results = pamr.cv(train, list(x = X_bootstrap, y = Y_bootstrap), nfold = 10);
   #SAVE THE MINIMUM VALIDATION ERROR
   test_error_array[i] = min(results$error);
+  
+  #save the gene names
+  #pamr.listgenes(train, list(x = X_bootstrap, y = Y_bootstrap), threshold=0.1, genenames=TRUE);
 }
 
 #print results
 print("Mean and standard deviation of the test error (%)");
 print(mean(test_error_array)*100);
 print(sd(test_error_array)*100);
+
+#plot running standard deviation to judge how many significant figures to use
+#runSD(test_error_array, n=1, cumulative=TRUE)[(n_repeat/2):n_repeat]
+plot(runSD(test_error_array, n=1, cumulative=TRUE)[(n_repeat/2):n_repeat],type="l")
+hist(runSD(test_error_array, n=1, cumulative=TRUE)[(n_repeat/2):n_repeat])
+print("Standard deviation of 2nd half of running standard deviations (%)");
+print(sd(runSD(test_error_array, n=1, cumulative=TRUE)[(n_repeat/2):n_repeat])*100);
+print("Estimate standard deviation of standard deviation (%)");
+print(sd(test_error_array)*100/sqrt(2*(n_repeat-1)));
+
+#save the variables
+save(list = ls(all.names = TRUE), file = "pam_classification_results_500.RData", envir = .GlobalEnv);
